4a77f0b53355b1a73bd545c0b1b335168bc4fc4b
scriptMang <aperalta686@gmail.com>
2026-03-18 08:17:55 -0400

    Remove center style from counter heading since it inherits the prop from the root container elem

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
   return (
     <>
      <div className="stepCounterCard">
-  <h2 style={{ textAlign: "center" }}>Counter</h2>
+  <h2>Counter</h2>
 
   <div style={{ fontSize: "2em", textAlign: "center", margin: "20px 0" }}>
     Current Count: <span style={{ fontWeight: "bold" }}>{count}</span>
